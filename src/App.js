b62225239d100c9dcbce248cb211aa9c824aec0f
iznor <iznor.hucuev9507@gmail.comm>
2023-01-23 16:33:28 +0300

    Сделал список пользователей с возможностью добавления их

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [invites, setInvites] = React.useState([]);
+  const [secces, setSucces] = React.useState(false);
+  React.useEffect(() => {
+    fetch(`https://reqres.in/api/users`)
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((error) => {
+        alert("Ощибка при подключении пользователей");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleOnClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+const handleClickInvites = () => {
+setSucces(true)
+}
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {secces ? (
+        <Success count={invites.length}  handleClickInvites={handleClickInvites} />
+      ) : (
+        <Users
+      handleClickInvites={handleClickInvites}
+          invites={invites}
+          handleOnClickInvite={handleOnClickInvite}
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          items={users}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 }
